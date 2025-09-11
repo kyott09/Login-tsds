e884ddcb965c5dc5f2e89185c9929541a91070ff
kyott09 <tobiasdinelli@gmail.com>
2025-09-11 19:44:30 -0300

    favicon hecho

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,7 +4,7 @@
   @stack('styles')
 
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/png" href="{{ asset('dist/img/FAVICON.png') }}" /> 
   <title>Plugin</title>
 
   <!-- Google Font: Source Sans Pro -->
