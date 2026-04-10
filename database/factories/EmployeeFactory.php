dfbf7f130b37a250106bea5c4a8dbe5f5afa43df
kyott09 <tobiasdinelli@gmail.com>
2026-04-10 20:23:18 -0300

    commit gallery solucion

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -13,18 +13,17 @@ class EmployeeFactory extends Factory
 
     public function definition()
     {
-        // Crear el usuario normalmente (sin intentar insertar 'role')
-        $user = User::factory()->create();
+        $faker = \Faker\Factory::create('es_AR');
 
-        // Asignar el rol "empleado" al usuario usando Spatie Laravel Permission
+        $user = User::factory()->create();
         $user->assignRole('empleado');
 
         return [
-            'user_id' => $user->id, // Usa el ID del usuario creado
+            'user_id' => $user->id,
             'vehicle_id' => Vehicle::inRandomOrder()->first()?->id ?? Vehicle::factory(),
-            'fecha_ingreso' => $this->faker->date(),
-            'skills' => $this->faker->words(3, true),
-            'estado_laboral' => $this->faker->randomElement(['activo', 'licencia', 'baja']),
+            'fecha_ingreso' => $faker->date(),
+            'skills' => $faker->words(3, true),
+            'estado_laboral' => $faker->randomElement(['activo', 'licencia', 'baja']),
             'fecha_inicio_licencia' => null,
             'fecha_fin_licencia' => null,
         ];
